e4818d883aa51facd6b2edabc4a604e0b6aab031
bobbidenisleo-glitch <bobbidenisleo@gmail.com>
2026-07-02 16:08:04 +0300

    Step 5: Add frontend build

---
+++ src/main/java/hexlet/code/app/config/WebConfig.java
@@ -0,0 +1,23 @@
+package hexlet.code.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
