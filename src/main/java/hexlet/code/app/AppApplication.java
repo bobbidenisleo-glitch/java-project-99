c7b1eecd9df256baa0f9326de3c864f766633cc1
bobbidenisleo-glitch <bobbidenisleo@gmail.com>
2026-07-11 19:54:59 +0300

    Step 10: Add Sentry initialization in AppApplication

--- src/main/java/hexlet/code/app/AppApplication.java
+++ src/main/java/hexlet/code/app/AppApplication.java
@@ -1,5 +1,6 @@
 package hexlet.code.app;
 
+import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class AppApplication {
 
     public static void main(String[] args) {
+        // Инициализация Sentry
+        Sentry.init(options -> {
+            options.setDsn(System.getenv("SENTRY_DSN"));
+            options.setSendDefaultPii(true);
+            options.setDebug(true);
+        });
+
         SpringApplication.run(AppApplication.class, args);
     }
 
