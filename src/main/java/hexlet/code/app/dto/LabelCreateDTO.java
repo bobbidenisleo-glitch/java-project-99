08299e5cc7962026bd3f5ca3e3275a67a043e070
bobbidenisleo-glitch <bobbidenisleo@gmail.com>
2026-07-12 13:59:53 +0300

    Fix DTOs: add NoArgsConstructor and AllArgsConstructor

--- src/main/java/hexlet/code/app/dto/LabelCreateDTO.java
+++ src/main/java/hexlet/code/app/dto/LabelCreateDTO.java
@@ -2,11 +2,15 @@ package hexlet.code.app.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LabelCreateDTO {
     @NotBlank
     @Size(min = 3, max = 1000)
