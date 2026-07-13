84122217369a5a56d577d17f724cdfa760fb862f
bobbidenisleo-glitch <bobbidenisleo@gmail.com>
2026-07-13 12:05:57 +0300

    Fix authenticationProvider and add Swagger documentation

--- src/main/java/hexlet/code/app/config/SecurityConfig.java
+++ src/main/java/hexlet/code/app/config/SecurityConfig.java
@@ -71,7 +71,6 @@ public class SecurityConfig {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
-        authProvider.setHideUserNotFoundExceptions(false);
         return authProvider;
     }
 
